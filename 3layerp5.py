fdcefc0dc43e01725769af2b0f40b2fba271c533
iadrien <xie.z@wustl.edu>
2018-09-08 21:20:18 -0500

    NN autoencoder

---
+++ 3layerp5.py
@@ -0,0 +1,90 @@
+from keras.layers import Input, Dense
+from keras import optimizers, initializers
+from keras.models import Model
+import time
+import csv
+import sys
+
+
+initWeight = float(sys.argv[1])
+inputlayer = Input(shape=(16,))
+
+
+encoded = Dense(5, activation='sigmoid',
+                kernel_initializer=initializers.Constant(value=initWeight),
+                bias_initializer='zero')(inputlayer)
+decoded = Dense(16, activation='sigmoid',
+                kernel_initializer=initializers.Constant(value=initWeight),
+                bias_initializer='zero')(encoded)
+
+autoencoder = Model(inputlayer, decoded)
+encoder = Model(inputlayer, encoded)
+
+encoded_input = Input(shape=(5,))
+decoder_layer = autoencoder.layers[-1]
+decoder = Model(encoded_input, decoder_layer(encoded_input))
+
+# autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
+
+rms = optimizers.RMSprop(lr=0.001, rho=0.9, epsilon=1e-07, decay=0.0)
+autoencoder.compile(optimizer=rms, loss='binary_crossentropy')
+
+x_train = [
+    [1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+        0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+    [0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+        0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+    [0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+        0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+    [0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0,
+        0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+    [0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0,
+        0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+    [0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0,
+        0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+    [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0,
+        0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+    [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0,
+        0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+    [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+        1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+    [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+        0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+    [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+        0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+    [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+        0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0],
+    [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+        0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0],
+    [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+        0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0],
+    [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+        0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0],
+    [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+        0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0]
+]
+
+start_time = time.time()
+
+autoencoder.fit(x_train, x_train,
+                nb_epoch=1500000,
+                verbose=2,
+                batch_size=256,
+                shuffle=True,
+                validation_data=(x_train, x_train))
+
+
+encoded_data = encoder.predict(x_train)
+print(encoded_data)
+decoded_data = decoder.predict(encoded_data)
+print(decoded_data)
+
+with open('3l5p_i%d_decoded.csv' % int(initWeight*100), 'w', newline='') as f:
+    wr = csv.writer(f)
+    wr.writerows(decoded_data)
+
+with open('3l5p_i%d_encoded.csv' % int(initWeight*100), 'w', newline='') as f:
+    wr = csv.writer(f)
+    wr.writerows(encoded_data)
+
+print("%f s" % (time.time() - start_time))
---
+++ 5layerAE_relu.py
@@ -0,0 +1,89 @@
+from keras.layers import Input, Dense
+from keras import optimizers,initializers
+from keras.models import Model
+
+
+
+
+input_data = Input(shape=(16,))
+print('RELU AE')
+encodedh = Dense(8, activation='relu',kernel_initializer=initializers.Constant(value=0.3),
+                bias_initializer='zero')(input_data)
+encoded = Dense(4, activation='relu',kernel_initializer=initializers.Constant(value=0.3),
+                bias_initializer='zero')(encodedh)
+decodedh = Dense(8, activation='relu',kernel_initializer=initializers.Constant(value=0.3),
+                bias_initializer='zero')(encoded)
+decoded = Dense(16, activation='relu',kernel_initializer=initializers.Constant(value=0.3),
+                bias_initializer='zero')(decodedh)
+
+
+autoencoder = Model(input_data, decoded)
+encoderh = Model(input_data, encodedh)
+encoder = Model(input_data, encoded)
+decoderh = Model(input_data, decodedh)
+
+
+
+# autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
+
+rms = optimizers.RMSprop(lr=0.0001, rho=0.9, epsilon=1e-08, decay=0.0)
+autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
+
+
+x_train = [[1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0],
+[0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0],
+[0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0],
+[0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0],
+[0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0],
+[0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0],
+[0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0],
+[0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0],
+[0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0],
+[0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0],
+[0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0],
+[0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0],
+[0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0],
+[0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0],
+[0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0],
+[0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0]]
+
+
+
+autoencoder.fit(x_train, x_train,
+                nb_epoch=5000000,
+                verbose=2,
+                batch_size=256,
+                shuffle=True,
+                validation_data=(x_train, x_train))
+
+
+half_encoded = encoderh.predict(x_train)
+print(half_encoded)
+
+
+encoded_data = encoder.predict(x_train)
+print(encoded_data)
+
+half_decoded = decoderh.predict(x_train)
+print(half_decoded)
+
+decoded_data = autoencoder.predict(x_train)
+print(decoded_data)
+
+with open('5l_i%dhalfencoded.csv' % int(initWeight*100), 'w', newline='') as f:
+    wr = csv.writer(f)
+    wr.writerows(half_encoded)
+
+with open('5l_i%d_encoded.csv' % int(initWeight*100), 'w', newline='') as f:
+    wr = csv.writer(f)
+    wr.writerows(encoded_data)
+
+with open('5l_i%d_halfdecoded.csv' % int(initWeight*100), 'w', newline='') as f:
+    wr = csv.writer(f)
+    wr.writerows(half_decoded)
+
+with open('5l_i%d_decoded.csv' % int(initWeight*100), 'w', newline='') as f:
+    wr = csv.writer(f)
+    wr.writerows(decoded_data)
+
+print('initial weight = %f' % initWeight)
